3ac5e4205a7ce0cde9f7a8b04a6a98c32d156d4a
Jenny Gardner <j.rongjie@gmail.com>
2020-02-08 15:21:29 -0500

    Lab2 assignment, previous message wrong, notes

---
+++ labs/2/src/index.js
@@ -0,0 +1,17 @@
+// Import reference to the Card Class
+import Card from "./components/Card/index.js";
+
+function runOnLoad(){    
+    // Create a container for us    
+    let element = document.createElement("div");    
+    element.id = "container";    
+
+    document.body.appendChild(element);    
+    var newTask = new Card({
+        content:"Sample value provided"
+    });    
+    
+    element.appendChild(newTask.render());
+}
+    
+    window.addEventListener ("DOMContentLoaded", runOnLoad);
