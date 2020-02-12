f81ee0eaee2cddd9b65c3e26c762c103dc33009c
Jenny Gardner <j.rongjie@gmail.com>
2020-02-12 11:50:35 -0500

    Updates

---
+++ precache-manifest.499392b9564cd91e8ccf4949c7182265.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b84ce10bfecbd5501d8c53bb2f12000c",
+    "url": "/dig4639-mobile-dev/index.html"
+  },
+  {
+    "revision": "024dffd954f6321ea066",
+    "url": "/dig4639-mobile-dev/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "0d0ae6ee3ad0d33d12a4",
+    "url": "/dig4639-mobile-dev/static/js/2.a0b43dfa.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/dig4639-mobile-dev/static/js/2.a0b43dfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "024dffd954f6321ea066",
+    "url": "/dig4639-mobile-dev/static/js/main.a618904e.chunk.js"
+  },
+  {
+    "revision": "170169ccdc7dff24daa3",
+    "url": "/dig4639-mobile-dev/static/js/runtime-main.e7bb69b4.js"
+  }
+]);
