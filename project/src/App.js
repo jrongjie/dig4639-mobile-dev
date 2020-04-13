f6caf51d6dbf5c6726374b060ae25f5516f0684d
Jenny Gardner <j.rongjie@gmail.com>
2020-04-13 17:46:28 -0400

    Added some code from lab 4

--- project/src/App.js
+++ project/src/App.js
@@ -1,22 +1,27 @@
 import React from 'react';
 
+//Create at least one component (React) or screen/component (React Native) for each major action the API provides:
+  //See all current contacts (/contacts)
+  //Add a new contact (/contacts/add)
+  //Remove a specific contact by position (/contacts/remove)
+  //See the current name and contacts length (/profile)
+  //All components can be on the same long page but must update dynamically if the list of contacts changes through accessing the adding or removing functionality.
+
+//All communication with the server MUST use window.fetch with a header defined as "API" and use the student's last name.
+
 class App extends React.Component {
 
   constructor(props) {
     super(props);
-
     this.state = {contacts: []};
-
   }
 
   componentDidMount() {
-
     window.fetch("http://plato.mrl.ai:8080/contacts", {headers: {API: "gardner"}})
     .then((res) => res.json())
     .then((data) => {
       this.setState({contacts: data.contacts});
     });
-
   }
 
   render() {
