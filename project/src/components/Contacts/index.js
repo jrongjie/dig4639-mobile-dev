efb2c70c445b6216829d5af9667a429a64336514
Jenny Gardner <j.rongjie@gmail.com>
2020-04-13 18:01:57 -0400

    fixed index.css figuring out profile-data.json

--- project/src/components/Contacts/index.js
+++ project/src/components/Contacts/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Profile from "../Profile/";
-import data from "../../data.json";
+import data from "../../profile-data.json";
 
 class Contacts extends React.Component {
   constructor(props) {
